f8a435eec5ac614d52c46be62c6f552cf77ae5bd
Valeria Montoya <valems1996@gmail.com>
2024-06-20 01:04:26 -0600

    Initial commit

---
+++ src/main/java/com/practica02sitioweb/Practica02SitioWebApplication.java
@@ -0,0 +1,13 @@
+package com.practica02sitioweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica02SitioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica02SitioWebApplication.class, args);
+	}
+
+}
